57bfd453f6ea935b8f56f778302815cbecd1f2bc
richardhamlyn <richardhamlyn@gmail.com>
2017-02-12 15:15:41 +0000

    Add project files.

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// Common assembly info shared by all projects/assemblies
+
+#if DEBUG
+    [assembly: AssemblyConfiguration("Debug")]
+#else
+    [assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: AssemblyCompany("SDL Group")]
+[assembly: AssemblyProduct("SDL Digital Experience Accelerator")]
+
+[assembly: ComVisible(false)]
+
+// NOTE: Version Info and Copyright statement is automatically appended by the build process (ciBuild.proj)
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.309")]
+[assembly: AssemblyInformationalVersion("1.7.0")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 SDL Group")]
